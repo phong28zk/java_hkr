0cd16df17fa57921913adb91fc9f96735df2c4af
phong28zk <PhongTN.B20VT288@stu.ptit.edu.vn>
2024-06-13 21:48:58 +0700

    Chore: add more

--- src/oop_prob/exception001.java
+++ src/oop_prob/exception001.java
@@ -1,67 +1,55 @@
-import java.lang.*;
-import java.util.Scanner;
 import java.io.*;
 import java.lang.*;
+import java.util.*;
+import java.math.*;
+
+class CurrencyMismatchException extends Exception {
+    public CurrencyMismatchException(String msg) {
+        super(msg);
+    }
+}
 
 class Amount {
-    public String currency;
-    public int amount;
+    String currency;
+    int amount;
     
     Amount(String currency, int amount) {
-        this.currency = currency;
+    	this.currency = currency;
         this.amount = amount;
     }
     
-    public void add(Amount add) throws CurrencyMismatchException {
-        if(!this.currency.equals(add.currency)) {
-            throw new CurrencyMismatchException("Currency doesn't match");
-        }
-        this.amount += add.amount;
+    public int getAmount() {
+        return this.amount;
     }
     
-    public int getAmount() {
-        return amount;
+    public void add(Amount other) throws CurrencyMismatchException {
+       	if (!this.currency.equals(other.currency)) {
+            throw new CurrencyMismatchException("Currency doesn't match");
+        } else {
+            this.amount += other.amount;
+        }
     }
 }
 
-class CurrencyMismatchException extends Exception {
-    public CurrencyMismatchException(String message) {
-        super(message);
-    }
-}
-public class exception001 {
-    public static void main(String args[]) {
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while(t-->0) {
-            String inp = sc.nextLine();
-            String[] line = inp.split(" ");
+        int tt = Integer.parseInt(sc.nextLine());
+        while(tt-->0) {
+            String line = sc.nextLine();
+            String[] inp = line.split(" ");
+            String c1 = inp[0];
+            int a1 = Integer.parseInt(inp[1]);
+            String c2 = inp[2];
+            int a2 = Integer.parseInt(inp[3]);
+            Amount am1 = new Amount(c1, a1);
+            Amount am2 = new Amount(c2, a2);
             try {
-                if(line.length != 4) {
-                    throw new Exception("Invalid Input");
-                }
-                String currency1 = line[0];
-                int amount1 = Integer.parseInt(line[1]);
-                String currency2 = line[2];
-                int amount2 = Integer.parseInt(line[3]);
-
-                Amount a1 = new Amount(currency1, amount1);
-                    Amount a2 = new Amount(currency2, amount2);
-
-                    try {
-                        if(a1.currency.equals(a2.currency)) {
-                            a1.add(a2);
-                            System.out.println(a1.getAmount());
-                        } else {
-                            throw new Exception("Currency doesn't match");
-                        }
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
-
-                } catch(Exception e) {
-                    System.out.println(e.getMessage());
-                }
+                am1.add(am2);
+            	System.out.println(am1.getAmount());
+            } catch(CurrencyMismatchException e) {
+				System.out.println(e.getMessage());
+            }
         }
     }
 }
